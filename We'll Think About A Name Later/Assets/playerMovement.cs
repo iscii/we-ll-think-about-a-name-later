68d46ad00fe4d83236a5d530ba96eaa7b552e2eb
iscii <issac.zheng9869@gmail.com>
2021-06-10 09:26:41 -0400

    init commit

---
+++ We'll Think About A Name Later/Assets/playerMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("w")){
+            gameObject.transform.Translate(new Vector2(0, 1));
+        }
+        if(Input.GetKey("a")){
+            gameObject.transform.Translate(new Vector2(-1, 0));
+        }
+        if(Input.GetKey("s")){
+            gameObject.transform.Translate(new Vector2(0, -1));
+        }
+        if(Input.GetKey("d")){
+            gameObject.transform.Translate(new Vector2(-1, 0));
+        }
+    }
+}
