dfe88805d31da65c71579952dc5ff9f689aa57eb
Dragy <qwu7127@bths.edu>
2021-06-16 12:27:12 -0400

    Update bossAction.cs

--- We'll Think About A Name Later/Assets/Scripts/bossAction.cs
+++ We'll Think About A Name Later/Assets/Scripts/bossAction.cs
@@ -10,8 +10,13 @@ public class bossAction : MonoBehaviour
     float[] shotIntervals = {1.25f, 1f, 0.75f, 0.5f, 0.25f};
     float time, camWidth, camHeight;
     bool phaseDone = true;
+<<<<<<< Updated upstream
     ArrayList phaseRequirement;
     Camera cam;
+||||||| constructed merge base
+=======
+    ArrayList phaseRequirement;
+>>>>>>> Stashed changes
     GameObject player;
     GameObject projectile;
     SpriteRenderer spriteRenderer;
@@ -27,9 +32,16 @@ public class bossAction : MonoBehaviour
         player = GameObject.FindGameObjectWithTag("Player");
         projectile = Resources.Load<GameObject>("Prefabs/ProjectileBoss");
         spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+<<<<<<< Updated upstream
 
         time = Time.time;
         phaseRequirement = new ArrayList();
+||||||| constructed merge base
+        lastShot = Time.time;
+=======
+        phaseRequirement = new ArrayList();
+        lastShot = Time.time;
+>>>>>>> Stashed changes
         initializePhase();
 
         //Sets the position of the boss outside the camera view
@@ -62,6 +74,7 @@ public class bossAction : MonoBehaviour
                 phase = Random.Range(0, idx);
                 phaseDone = false;
             }
+<<<<<<< Updated upstream
 
             switch(phase) {
                 case 0:
@@ -71,10 +84,28 @@ public class bossAction : MonoBehaviour
                     phase2();
                 break;
             }
+||||||| constructed merge base
+            phase = Random.Range(0, idx);
+            switch(phase) {
+                case 0:
+                    phase1();
+                break;
+            }
+            phaseDone = false;
+=======
+            phase = Random.Range(0, idx);
+            phaseDone = false;
+>>>>>>> Stashed changes
+        }
+        switch(phase) {
+            case 0:
+                phase1();
+            break;
         }
     }
 
     void phase1() {
+<<<<<<< Updated upstream
         if(Time.time - time > shotIntervals[shotIdx]) {
             transform.position = new Vector2(player.transform.position.x, transform.position.y);
             Instantiate(projectile, new Vector3(transform.position.x, transform.position.y - Mathf.Ceil(spriteRenderer.bounds.size.y/2) - 0.1f, transform.position.z), transform.rotation);
@@ -83,11 +114,40 @@ public class bossAction : MonoBehaviour
             if(shots == shotsToChange && shotIdx < shotIntervals.Length - 1) {
                 shots = 0;
                 shotIdx++;
+||||||| constructed merge base
+        while(!phaseDone) {
+            if(Time.time - lastShot > shotIntervals[shotIdx]) {
+                transform.position = new Vector2(player.transform.position.x, transform.position.y);
+                Instantiate(projectile, new Vector3(transform.position.x, transform.position.y - Mathf.Ceil(spriteRenderer.bounds.size.y/2) - 0.1f, transform.position.z), transform.rotation);
+                shots++;
+                lastShot = Time.time;
+                if(shots == shotsToChange && shotIdx < shotIntervals.Length - 1) {
+                    shots = 0;
+                    shotIdx++;
+                }
+                if(shotIdx >= 25)
+                phaseDone = true;
+=======
+        if(Time.time - lastShot > shotIntervals[shotIdx]) {
+            transform.position = new Vector2(player.transform.position.x, transform.position.y);
+            Instantiate(projectile, new Vector3(transform.position.x, transform.position.y - Mathf.Ceil(spriteRenderer.bounds.size.y/2) - 0.1f, transform.position.z), transform.rotation);
+            shots++;
+            lastShot = Time.time;
+            if(shots == shotsToChange && shotIdx < shotIntervals.Length - 1) {
+                shots = 0;
+                shotIdx++;
+>>>>>>> Stashed changes
             }
+<<<<<<< Updated upstream
             Debug.Log(shotIdx); //shotIdx goes up to 4 after a while
             if(shotIdx >= 25)
                 phaseDone = true;
             
+||||||| constructed merge base
+=======
+            if(shotIdx >= 25)
+                phaseDone = true;
+>>>>>>> Stashed changes
         }
     }
 
