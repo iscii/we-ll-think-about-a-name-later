c7bb910b9b7819dac32cf878b47db3f3acc3c254
Dragy <qwu7127@bths.edu>
2021-06-16 12:26:00 -0400

    Added beginning boss SHOWDOWN

--- We'll Think About A Name Later/Assets/Scripts/bossAction.cs
+++ We'll Think About A Name Later/Assets/Scripts/bossAction.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class bossAction : MonoBehaviour
 {
     public int shots = 0, shotsToChange = 5;
+    public bool canMove = false;
     int phase, shotIdx = 0;
-    ArrayList phaseRequirement;
     float[] shotIntervals = {1.25f, 1f, 0.75f, 0.5f, 0.25f};
-    float lastShot;
+    float time, camWidth, camHeight;
     bool phaseDone = true;
+    ArrayList phaseRequirement;
+    Camera cam;
     GameObject player;
     GameObject projectile;
     SpriteRenderer spriteRenderer;
@@ -17,46 +19,67 @@ public class bossAction : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //gets the radius of the height and width of the camera borders
+        cam = Camera.main;
+        camHeight = cam.orthographicSize;
+        camWidth = camHeight * cam.aspect;
+
         player = GameObject.FindGameObjectWithTag("Player");
         projectile = Resources.Load<GameObject>("Prefabs/ProjectileBoss");
         spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+
+        time = Time.time;
         phaseRequirement = new ArrayList();
-        lastShot = Time.time;
         initializePhase();
+
+        //Sets the position of the boss outside the camera view
+        transform.position = new Vector2(player.transform.position.x, Mathf.Ceil(camHeight + spriteRenderer.bounds.size.y/2f));
+        Debug.Log(transform.position.y);
     }
 
     void initializePhase() {
-        phaseRequirement.Add(0);
+        phaseRequirement.Add(0); //phase1
+        phaseRequirement.Add(0); //phase2
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(phaseDone) {
-            int idx = 0;
-            for(int i=0; i<phaseRequirement.Count; i++) {
-                if(shots <= (int)(phaseRequirement[i])) {
-                    idx = i; 
-                    break;
-                }
-            }
-            phase = Random.Range(0, idx);
-            Debug.Log(phase);
-            phaseDone = false;
+        if(Time.time - time >= 3f && !canMove) {
+            transform.position = new Vector2(transform.position.x, transform.position.y - 0.01f);
+            if(transform.position.y <= camHeight)
+                canMove = true;
         }
-        switch(phase) {
-            case 0:
-                phase1();
-            break;
+        if(canMove) {
+            if(phaseDone) {
+                int idx = 0;
+                for(int i=0; i<phaseRequirement.Count; i++) {
+                    if(shots < (int)(phaseRequirement[i])) {
+                        idx = i; 
+                        break;
+                    }
+                }
+                phase = Random.Range(0, idx);
+                phaseDone = false;
+            }
+
+            switch(phase) {
+                case 0:
+                    phase1();
+                break;
+                case 1:
+                    phase2();
+                break;
+            }
         }
     }
 
     void phase1() {
-        if(Time.time - lastShot > shotIntervals[shotIdx]) {
+        if(Time.time - time > shotIntervals[shotIdx]) {
             transform.position = new Vector2(player.transform.position.x, transform.position.y);
             Instantiate(projectile, new Vector3(transform.position.x, transform.position.y - Mathf.Ceil(spriteRenderer.bounds.size.y/2) - 0.1f, transform.position.z), transform.rotation);
             shots++;
-            lastShot = Time.time;
+            time = Time.time;
             if(shots == shotsToChange && shotIdx < shotIntervals.Length - 1) {
                 shots = 0;
                 shotIdx++;
@@ -66,5 +89,9 @@ public class bossAction : MonoBehaviour
                 phaseDone = true;
             
         }
+    }
+
+    void phase2() {
+        
     }
 }
