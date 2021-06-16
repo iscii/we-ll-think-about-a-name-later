d1c25fbdbb4ae1e7763e2b5457000e15130c818f
iscii <issac.zheng9869@gmail.com>
2021-06-15 20:23:43 -0400

    improve movement

--- We'll Think About A Name Later/Assets/Scripts/playerAction.cs
+++ We'll Think About A Name Later/Assets/Scripts/playerAction.cs
@@ -26,21 +26,25 @@ public class playerAction : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("w"))
-        {
-            transform.Translate(new Vector2(0, 1) * ms * Time.deltaTime);
+        if(!(Input.GetKey("w") && Input.GetKey("s"))){
+            if (Input.GetKey("w"))
+            {
+                transform.Translate(new Vector2(0, 1) * ms * Time.deltaTime);
+            }
+            if (Input.GetKey("s"))
+            {
+                transform.Translate(new Vector2(0, -1) * ms * Time.deltaTime);
+            }
         }
-        else if (Input.GetKey("s"))
-        {
-            transform.Translate(new Vector2(0, -1) * ms * Time.deltaTime);
-        }
-        if (Input.GetKey("a"))
-        {
-            transform.Translate(new Vector2(-1, 0) * ms * Time.deltaTime);
-        }
-        else if (Input.GetKey("d"))
-        {
-            transform.Translate(new Vector2(1, 0) * ms * Time.deltaTime);
+        if(!(Input.GetKey("a") && Input.GetKey("d"))){
+            if (Input.GetKey("a"))
+            {
+                transform.Translate(new Vector2(-1, 0) * ms * Time.deltaTime);
+            }
+            if (Input.GetKey("d"))
+            {
+                transform.Translate(new Vector2(1, 0) * ms * Time.deltaTime);
+            }
         }
 
         if (Input.GetKey("r"))
