6fc95fbcabdd9eb6757bb3199022b78fbe619dd1
iscii <issac.zheng9869@gmail.com>
2021-06-16 12:55:40 -0400

    Reverting to https://medium.com/swlh/using-git-how-to-go-back-to-a-previous-commit-8579ccc8180f

--- We'll Think About A Name Later/Assets/Scripts/playerAction.cs
+++ We'll Think About A Name Later/Assets/Scripts/playerAction.cs
@@ -4,36 +4,47 @@ using UnityEngine;
 
 public class playerAction : MonoBehaviour
 {
-    int ms = 5, hp = 4;
-    GameObject proj;
-    SpriteRenderer spriteRenderer;
+    int ms = 5, hp = 5;
     float lastRotateInput, lastShotInput, timeBetweenInputs = 0.2f, shotInterval = 0.5f;
+    Sprite[] hpSprites;
+    GameObject proj;
+    SpriteRenderer playerSR, hpSR;
+    
     // Start is called before the first frame update
     void Start()
     {
         proj = Resources.Load<GameObject>("Prefabs/playerProjectile");
-        spriteRenderer = transform.GetChild(0).GetComponent<SpriteRenderer>();
+        playerSR = transform.GetChild(0).GetComponent<SpriteRenderer>();
+        hpSR = transform.GetChild(1).GetComponent<SpriteRenderer>();
+        hpSprites = new Sprite[4];
+        for(int i=0;i<4;i++){
+            hpSprites[i] = Resources.Load<Sprite>("Sprites/" + i.ToString());
+        }
         lastRotateInput = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("w"))
-        {
-            transform.Translate(new Vector2(0, 1) * ms * Time.deltaTime);
+        if(!(Input.GetKey("w") && Input.GetKey("s"))){
+            if (Input.GetKey("w"))
+            {
+                transform.Translate(new Vector2(0, 1) * ms * Time.deltaTime);
+            }
+            if (Input.GetKey("s"))
+            {
+                transform.Translate(new Vector2(0, -1) * ms * Time.deltaTime);
+            }
         }
-        else if (Input.GetKey("s"))
-        {
-            transform.Translate(new Vector2(0, -1) * ms * Time.deltaTime);
-        }
-        if (Input.GetKey("a"))
-        {
-            transform.Translate(new Vector2(-1, 0) * ms * Time.deltaTime);
-        }
-        else if (Input.GetKey("d"))
-        {
-            transform.Translate(new Vector2(1, 0) * ms * Time.deltaTime);
+        if(!(Input.GetKey("a") && Input.GetKey("d"))){
+            if (Input.GetKey("a"))
+            {
+                transform.Translate(new Vector2(-1, 0) * ms * Time.deltaTime);
+            }
+            if (Input.GetKey("d"))
+            {
+                transform.Translate(new Vector2(1, 0) * ms * Time.deltaTime);
+            }
         }
 
         if (Input.GetKey("r"))
@@ -51,7 +62,7 @@ public class playerAction : MonoBehaviour
             {
                 //fix rotation shooting
                 //multiply the size by the rotation vector (-1 or 1) when rotated.
-                Instantiate(proj, new Vector3(transform.position.x, transform.position.y + Mathf.Ceil(spriteRenderer.bounds.size.y/2) + 0.1f, transform.position.z), transform.GetChild(0).rotation);
+                Instantiate(proj, new Vector3(transform.position.x, transform.position.y + Mathf.Ceil(playerSR.bounds.size.y/2) + 0.1f, transform.position.z), transform.GetChild(0).rotation);
                 lastShotInput = Time.time;
             }
         }
@@ -59,8 +70,8 @@ public class playerAction : MonoBehaviour
 
     public void takeDamage(){
         hp--;
-        Debug.Log("Player HP: " + hp);
+        hpSR.sprite = hpSprites[hp > 0 ? hp-1 : 0];
         if(hp <= 0)
-            Debug.Log("You have died");
+            Debug.Log("You have died"); //not implementing yet for testing purposes
     }
 }
