9fc0deded3b39bfc4675cbbde72e20c6d6f4aaf5
Dragy <qwu7127@bths.edu>
2021-06-26 20:17:03 -0400

    Put the player and boss classes into inheritance

--- We'll Think About A Name Later/Assets/Scripts/entity.cs
+++ We'll Think About A Name Later/Assets/Scripts/entity.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class entity : MonoBehaviour {
-    protected float camHeight, camWidth;
+    //variables to be shared with subclasses
+    protected const int projSpawn = 0;
+    protected float camHeight, camWidth, time, shotInterval;
     protected GameObject player, projectile;
     protected SpriteRenderer sr;
 
-    private const int projSpawn = 0;
-    private Camera cam;
+    //personal variables
+    Camera cam;
 
-    //Start is called before the first fram update
-    /* public void Start() {
+    // Start is called before the first frame update
+    void Start() {
         //gets the width and height of the camera view borders
         cam = Camera.main;
         camHeight = cam.orthographicSize;
@@ -20,26 +22,39 @@ public class entity : MonoBehaviour {
         player = GameObject.FindGameObjectWithTag("Player");
         projectile = Resources.Load<GameObject>("Prefabs/playerProjectile");
 
-    } 
+        time = Time.time;
+    }
+
+    //sets shotInterval based on whether it's the boss or the player
+    protected virtual void setShotInterval(float tempShotInterval) {
+        shotInterval = tempShotInterval;
+    }
     
-    //sets sr to a specific sprite : boss or the player
-    public void setSR(SpriteRenderer tempSR) {
+    //sets sr to a specific sprite : either the boss or the player
+    protected virtual void setSR(SpriteRenderer tempSR) {
         sr = tempSR;
     }
     
+    // Update is called once per frame
     private void Update() {
         checkState();
     }
     
+    //Child classes will override this method to fulfill their intented purposes
     protected virtual void checkState() {
 
-    }*/
+    }
 
-    /* void fireShot(Vector3 shotpos, Quaternion angle) {
-        Instantiate()
-    } */
+    //fireShot if you don't want to change position of the sprite
+    protected virtual void fireShot(Vector3 shotPos, Quaternion shotAngle) {
+        Instantiate(projectile, shotPos, shotAngle);
+        time = Time.time;
+    }
 
-    /* public void fireShot(Vector2 pos, Quaternion angle) {
-
-    } */
+    //fireShot if you want to change the position of the sprite before firing
+    protected virtual void fireShot(Vector2 pos, Quaternion angle, Vector3 shotPos, Quaternion shotAngle) {
+        transform.SetPositionAndRotation(pos, angle);
+        Instantiate(projectile, shotPos, shotAngle);
+        time = Time.time;
+    }
 }
--- We'll Think About A Name Later/Assets/Scripts/playerBehavior.cs
+++ We'll Think About A Name Later/Assets/Scripts/playerBehavior.cs
@@ -4,5 +4,92 @@ using UnityEngine;
 
 public class playerBehavior : entity
 {
+    private const int sprite = 0, hpBar = 1;
+    private float rotateTime, rotateInterval;
+    private GameObject boss;
+    private SpriteRenderer hpSR;
+    private Sprite[] hpSprites;
+    [SerializeField] int ms, hp;
 
+    //initializes all the variables 
+    private void Awake() {
+        boss = GameObject.FindGameObjectWithTag("Boss");
+        hpSR = transform.GetChild(hpBar).GetComponent<SpriteRenderer>();
+        hpSprites = new Sprite[4];
+        for (int i = 0; i < 4; i++)
+        {
+            hpSprites[i] = Resources.Load<Sprite>("Sprites/" + i.ToString());
+        }
+
+        ms = 7;
+        hp = 5;
+        rotateInterval = 0.2f;
+        rotateTime = Time.time;
+
+        setShotInterval(0.5f);
+        setSR(transform.GetChild(sprite).GetComponent<SpriteRenderer>());
+    }
+
+    //checks the input and activate its respectective actions
+    protected override void checkState()
+    {
+        if (boss.GetComponent<bossAction>().canMove)
+        {
+            if (!(Input.GetKey("w") && Input.GetKey("s")))
+            {
+                if (Input.GetKey("w") && ((transform.position.y + sr.bounds.size.y/2) < camHeight))
+                {
+                    transform.Translate(new Vector2(0, 1) * ms * Time.deltaTime);
+                }
+                if (Input.GetKey("s") && ((transform.position.y - sr.bounds.size.y/2) > (camHeight*-1)))
+                {
+                    transform.Translate(new Vector2(0, -1) * ms * Time.deltaTime);
+                }
+            }
+            if (!(Input.GetKey("a") && Input.GetKey("d")))
+            {
+                if (Input.GetKey("a") && ((transform.position.x - sr.bounds.size.x/2) > (camWidth*-1)))
+                {
+                    transform.Translate(new Vector2(-1, 0) * ms * Time.deltaTime);
+                }
+                if (Input.GetKey("d") && ((transform.position.x + sr.bounds.size.x/2) < camWidth))
+                {
+                    transform.Translate(new Vector2(1, 0) * ms * Time.deltaTime);
+                }
+            }
+            if (Input.GetKey("q"))
+            {
+                if (Time.time - rotateTime >= rotateInterval)
+                {
+                    transform.GetChild(sprite).Rotate(0, 0, 90);
+                    rotateTime = Time.time;
+                }
+            }
+            if (Input.GetKey("r"))
+            {
+                if (Time.time - rotateTime >= rotateInterval)
+                {
+                    transform.GetChild(sprite).Rotate(0, 0, -90);
+                    rotateTime = Time.time;
+                }
+            }
+
+            //TODO hitting the boss adds score
+            if (Input.GetKey("space"))
+            {
+                if (Time.time - time >= shotInterval)
+                {
+                    fireShot(transform.GetChild(sprite).GetChild(projSpawn).position, transform.GetChild(sprite).rotation);
+                }
+            }
+        }
+    }
+
+    public void takeDamage() {
+        hp--;
+        hpSR.sprite = hpSprites[hp > 0 ? hp - 1 : 0];
+        //TODO not implementing yet for testing purposes
+        if (hp <= 0)
+            Debug.Log("You have died");
+    }
 }
